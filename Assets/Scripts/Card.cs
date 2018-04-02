408e9fad7bc40d02e41c878523185c8fc972d649
DESKTOP-V1IN6J1\owner <vitor_kern@yahoo.com.br>
2018-04-01 22:50:51 -0300

    Inicio do Projeto
    Scriptable Objects e Drop and Drag de cartas.

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "New Card", menuName = "Card")]
+public class Card : ScriptableObject {
+
+    public string title;
+    public string description;
+
+    public Sprite artWork;
+
+    public int type;
+
+    public int servantAlt;
+    public int influeneAlt;
+    public int otherValue;
+}
